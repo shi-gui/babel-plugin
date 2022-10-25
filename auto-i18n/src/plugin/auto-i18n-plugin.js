142f7651dd197ddc94435ae48f2650b890601441
liuhua <1670835059@qq.com>
2022-10-25 11:50:53 +0800

    feat: 完善自动国际化插件

--- auto-i18n/src/plugin/auto-i18n-plugin.js
+++ auto-i18n/src/plugin/auto-i18n-plugin.js
@@ -1,11 +1,51 @@
 const { declare } = require("@babel/helper-plugin-utils");
+const fse = require('fs-extra');
+const path = require('path');
+const generate = require('@babel/generator').default;
 
+// 生成不同的语言包中唯一key
+let intlIndex = 0;
+function nextIntlKey() {
+  ++intlIndex;
+  return `intl${intlIndex}`;
+}
 const autoTrackPlugin = declare((api, options, dirname) => {
   api.assertVersion(7);
 
+  // 生成替换节点
+  function getReplaceExpression(path, value, intlUid) {
+    const expressionParams = path.isTemplateLiteral()
+      ? path.node.expressions.map((item) => generate(item).code)
+      : null;
+    let replaceExpression = api.template.ast(
+      `${intlUid}.t('${value}'${
+        expressionParams ? "," + expressionParams.join(",") : ""
+      })`
+    ).expression;
+    if (
+      path.findParent((p) => p.isJSXAttribute()) &&
+      !path.findParent((p) => p.isJSXExpressionContainer())
+    ) {
+      replaceExpression = api.types.JSXExpressionContainer(replaceExpression);
+    }
+    return replaceExpression;
+  }
+
+  // 收集替换的key和value， 保存到file中
+  function save(file, key, value) {
+    const allText = file.get("allText");
+    allText.push({
+      key,
+      value,
+    });
+    file.set("allText", allText);
+  }
+
   return {
     // 遍历visitor前调用
-    pre(file) {},
+    pre(file) {
+      file.set('allText', []);
+    },
 
     visitor: {
       Program: {
@@ -50,9 +90,59 @@ const autoTrackPlugin = declare((api, options, dirname) => {
           });
         },
       },
+      // StringLiteral 和 TemplateLiteral 节点，用 state.intlUid + '.t' 的函数调用语句来替换原节点
+      // 替换完以后要用 path.skip 跳过新生成节点的处理，不然就会进入无限循环
+      StringLiteral(path, state) {
+        if (path.node.skipTransform) {
+          return;
+        }
+        let key = nextIntlKey();
+        save(state.file, key, path.node.value);
+
+        const replaceExpression = getReplaceExpression(
+          path,
+          key,
+          state.intlUid
+        );
+        path.replaceWith(replaceExpression);
+        path.skip();
+      },
+      // 模版字符串需要吧 ${} 表达式的部分替换为 {placeholder} 的占位字符串。
+      TemplateLiteral(path, state) {
+        if (path.node.skipTransform) {
+          return;
+        }
+        const value = path
+          .get("quasis")
+          .map((item) => item.node.value.raw)
+          .join("{placeholder}");
+        if (value) {
+          let key = nextIntlKey();
+          save(state.file, key, value);
+
+          const replaceExpression = getReplaceExpression(
+            path,
+            key,
+            state.intlUid
+          );
+          path.replaceWith(replaceExpression);
+          path.skip();
+        }
+      },
     },
     // 遍历visitor后调用
-    post(file) {},
+    post(file) {
+      const allText = file.get('allText');
+      const intlData = allText.reduce((obj, item) => {
+          obj[item.key] = item.value;
+          return obj;
+      }, {});
+
+      const content = `const resource = ${JSON.stringify(intlData, null, 4)};\nexport default resource;`;
+      fse.ensureDirSync(options.outputDir);
+      fse.writeFileSync(path.join(options.outputDir, 'zh_CN.js'), content);
+      fse.writeFileSync(path.join(options.outputDir, 'en_US.js'), content);
+    },
   };
 });
 
